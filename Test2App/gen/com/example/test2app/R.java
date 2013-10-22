859a68a782bde9031fc6defc8c994a11972c2b3a
sheenaperea <srdperea@gmail.com>
2013-10-22 14:41:01 -0700

    Extra stuff

--- Test2App/gen/com/example/test2app/R.java
+++ Test2App/gen/com/example/test2app/R.java
@@ -27,7 +27,13 @@ public final class R {
         public static final int font_size=0x7f050002;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int dialer_icon=0x7f020000;
+        public static final int dialer_icon_pressed=0x7f020001;
+        public static final int emergency_icon=0x7f020002;
+        public static final int emergency_icon_pressed=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int map_icon=0x7f020005;
+        public static final int map_icon_pressed=0x7f020006;
     }
     public static final class id {
         public static final int TableLayout1=0x7f090000;
