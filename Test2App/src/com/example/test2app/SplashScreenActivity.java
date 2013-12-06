a8e1ec522005d8675cac9e49a46018963d2f6fbc
sheenaperea <srdperea@gmail.com>
2013-12-05 21:51:43 -0800

    Added Splash Screen

---
+++ Test2App/src/com/example/test2app/SplashScreenActivity.java
@@ -0,0 +1,30 @@
+package com.example.test2app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class SplashScreenActivity extends Activity {
+
+	private static int SPLASH_TIME_OUT = 3000;
+	 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+ 
+        new Handler().postDelayed(new Runnable() {
+ 
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+ 
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+ 
+}
